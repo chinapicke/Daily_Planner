dab55808c6262558949112199bc81e44aacfab9a
chinapicke <chinapickering@hotmail.co.uk>
2023-01-20 20:56:52 +0700

    added rows in main container, time of day and lock icon on HTML

--- starter/dailyplanner.js
+++ starter/dailyplanner.js
@@ -4,3 +4,8 @@ var todayDate = moment().format('dddd, MMMM Do');
 // Get element to put the current day into from the HTML 
 $('#currentDay').text(todayDate);
 
+// Date Displaty Blocks
+// Need to create ul with li elements inside 
+
+// Format with using class for the grid system, approx date will take up 1/5 row, input 3/5 and save button 1/5
+// Use current hour using moment
